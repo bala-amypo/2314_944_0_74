c97261484195387215f8f9d116f3cae0d7e3baa2
bala-amypo <bala@amypo.in>
2025-12-17 06:11:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,3 +1,40 @@
-public class StudentController{
-    
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.service.StudentService;
+import java.util.*;
+@RestController
+public class StudentController {
+    @Autowired
+    StudentService src;
+    @PostMapping("/post")
+    public StudentEntity postData(@RequestBody StudentEntity st){
+        return src.savedata(st);
+    }
+    @GetMapping("/getting")
+    public List<StudentEntity> getData(){
+        return src.retdata();
+    }
+    @GetMapping("/getid/{id}")
+    public StudentEntity sepData(@PathVariable int id){
+        return src.indData(id);
+
+    }
+    @PutMapping("/update/{id}")
+    public StudentEntity updateData(@PathVariable int id,@RequestBody StudentEntity st){
+        return src.updat(id,st);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void del(@PathVariable int id){
+         src.delete(id);
+    }
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -3,8 +3,6 @@ import java.util.List;
 import java.util.Optional;
 import com.example.demo.entity.StudentEntity;
 
-
-
 public interface StudentService{
     StudentEntity postdata(StudentEntity st);
     List<StudentEntity> getdata();
