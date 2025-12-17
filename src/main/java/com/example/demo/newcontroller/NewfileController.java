e08dfa80e82790b01293b46e22192b64b881dd53
bala-amypo <bala@amypo.in>
2025-12-17 10:06:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/newcontroller/NewfileController.java
@@ -0,0 +1,15 @@
+package com.example.demo.newcontroller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.newentity.NewfileEntity;
+import com.example.demo.newservice.NewfileService;
+public class NewfileController{
+    
+}
