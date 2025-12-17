92975505ef7edef873287de708f4f0bca3123002
bala-amypo <bala@amypo.in>
2025-12-17 04:34:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,40 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Studententity;
+import com.example.demo.service.Studentservice;
+import java.util.*;
 @RestController
-public class StudentController{
-    
+public class Studentcontroller {
+    @Autowired
+    Studentservice src;
+    @PostMapping("/post")
+    public Studententity postData(@RequestBody Studententity st){
+        return src.savedata(st);
+    }
+    @GetMapping("/getting")
+    public List<Studententity> getData(){
+        return src.retdata();
+    }
+    @GetMapping("/getid/{id}")
+    public Studententity sepData(@PathVariable int id){
+        return src.indData(id);
+
+    }
+    @PutMapping("/update/{id}")
+    public Studententity updateData(@PathVariable int id,@RequestBody Studententity st){
+        return src.updat(id,st);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void del(@PathVariable int id){
+         src.delete(id);
+    }
 }
--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,3 +1,30 @@
-public class StudentEntity{
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+public class Studententity {
+    private String name;
+    private int id;
+    private LocalDate dob;
+    private float cgpa;
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public Studententity(String name, int id, LocalDate dob, float cgpa) {
+        this.name = name;
+        this.id = id;
+        this.dob = dob;
+        this.cgpa = cgpa;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
     
+        
 }
